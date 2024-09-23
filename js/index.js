66c30e853c7a2439b026c23852183853065d4d81
Bappi1234 <harunbappi1234@gmail.com>
2024-09-23 22:46:27 +0600

    history button toggle added in JS

--- js/index.js
+++ js/index.js
@@ -1,61 +1,147 @@
-// Donation Now Button Js Here
-const donationNowBtnEl = document.getElementById("donation-now-btn");
-donationNowBtnEl.addEventListener("click", function () {
-  const donationFlood = parseFloat(
-    document.getElementById("donation-flood").value
-  );
-  if (isNaN(donationFlood)) {
-    alert("input field is empty");
+// Global Veriable Declare
+const gridContainerEl = document.getElementById("grid-container");
+const mainBalance = getInputTextByValue("main-balance");
+// global Veriable Declare of part History
+const historyContainer = document.getElementById("history-container");
+const historyText = document.getElementById("flood-noakhali-text").innerText;
+const historyText2 = document.getElementById("flood-relief-text").innerText;
+const historyText3 = document.getElementById("injured-quota-text").innerText;
+// Donate For Flood Button in JS
+const donateNowButtonEl = document.getElementById("donation-now-btn");
+donateNowButtonEl.addEventListener("click", function () {
+  // Select Input Field by Share Function
+  const donationFlood = getInputFieldByValue("donation-flood");
+  //   Condition Part
+  if (
+    isNaN(donationFlood) ||
+    mainBalance < donationFlood ||
+    donationFlood <= 0
+  ) {
+    alert("wrong amount! Please try Again");
     return;
   }
+  //   Add Donation Amount
   const floodBalanceEl = parseFloat(
     document.getElementById("flood-balance").innerText
   );
   const floodBalance = donationFlood + floodBalanceEl;
-  document.getElementById("flood-balance").innerText = floodBalance;
-  //   Decrement Total Balance
-  const totalBalanceEl = parseFloat(
-    document.getElementById("total-balance").innerText
+  document.getElementById("flood-balance").innerText = floodBalance.toFixed(2);
+  //   Decrease Main balance
+  const totalBalance1 = mainBalance - floodBalance;
+  document.getElementById("main-balance").innerText = totalBalance1.toFixed(2);
+  //   Open Modal
+  const myModal = document.getElementById("my_modal_5");
+  const donationBalance1 = document.getElementById("donation-balance-1");
+  donationBalance1.innerHTML = `${donationFlood.toFixed(2)} BDT`;
+  myModal.classList.remove("hidden");
+
+  //   Donation History
+  const historyContent = document.createElement("div");
+  historyContent.className = "border rounded-md p-5 mb-3";
+  historyContent.innerHTML = `
+  <h1 class="text-xl font-semibold">${donationFlood} Taka <span>${historyText}</span> </h1>
+  <p class="text-gray-500 text-xs">Date: ${new Date().toDateString()}
+     <span>${new Date().toTimeString()}</span></p>
+  `;
+  historyContainer.appendChild(historyContent);
+});
+//
+// Donation relief Button
+const donateNowButtonEl2 = document.getElementById("donation-now-btn-2");
+donateNowButtonEl2.addEventListener("click", function () {
+  // Select Input Field by Share Function
+  const donationFloodRelief = getInputFieldByValue("donatetion-flood-relief");
+  //   Condition Part
+  if (
+    isNaN(donationFloodRelief) ||
+    mainBalance < donationFloodRelief ||
+    donationFloodRelief <= 0
+  ) {
+    alert("wrong amount! Please try Again");
+    return;
+  }
+  //   Relief Balance Declare
+  const reliefBalanceEl = parseFloat(
+    document.getElementById("relief-balance").innerText
   );
-  const totalBalance = totalBalanceEl - donationFlood;
-  document.getElementById("total-balance").innerText = totalBalance;
-  window.location.re;
+  //   Add Donation Amount
+  const reliefBalance = donationFloodRelief + reliefBalanceEl;
+  document.getElementById("relief-balance").innerText = reliefBalance;
+  //   Decrease Main balance
+  const totalBalance2 = mainBalance - reliefBalance;
+  document.getElementById("main-balance").innerText = totalBalance2;
+  //   Open Modal
+  const myModal = document.getElementById("my_modal_2");
+  const donationBalance2 = document.getElementById("donation-balance-2");
+  donationBalance2.innerHTML = `${donationFloodRelief.toFixed(2)} BDT`;
+  myModal.classList.remove("hidden");
+  //   Donation History
+  const historyContent = document.createElement("div");
+  historyContent.className = "border rounded-md p-5 mb-3";
+  historyContent.innerHTML = `
+    <h1 class="text-xl font-semibold">${donationFloodRelief} Taka <span>${historyText2}</span> </h1>
+    <p class="text-gray-500 text-xs">Date: ${new Date().toDateString()}
+       <span>${new Date().toTimeString()}</span></p>
+    `;
+  historyContainer.appendChild(historyContent);
+});
+// Injured Quota Button
+const donateNowButtonEl3 = document.getElementById("donation-now-btn-3");
+donateNowButtonEl3.addEventListener("click", function () {
+  // Select Input Field by Share Function
+  const donationQuotaInjurd = getInputFieldByValue("donation-quota-injured");
+  //   Condition Part
+  if (
+    isNaN(donationQuotaInjurd) ||
+    mainBalance < donationQuotaInjurd ||
+    donationQuotaInjurd <= 0
+  ) {
+    alert("wrong amount! Please try Again");
+    return;
+  }
+  //   donation-quota-injured Declare
+  const injuredQuotaBalanceEl = parseFloat(
+    document.getElementById("injured-quota-balance").innerText
+  );
+  //   Add Donation Amount
+  const injuredQuotaBalance = donationQuotaInjurd + injuredQuotaBalanceEl;
+  document.getElementById("injured-quota-balance").innerText =
+    injuredQuotaBalance;
+  //   Decrease Main balance
+  const totalBalance3 = mainBalance - injuredQuotaBalance;
+  document.getElementById("main-balance").innerText = totalBalance3;
+  //   Open Modal
+  const myModal = document.getElementById("my_modal_3");
+  const donationBalance3 = document.getElementById("donation-balance-3");
+  donationBalance3.innerHTML = `${donationQuotaInjurd.toFixed(2)} BDT`;
+  myModal.classList.remove("hidden");
+  //   Donation History
+  const historyContent = document.createElement("div");
+  historyContent.className = "border rounded-md p-5 mb-3";
+  historyContent.innerHTML = `
+     <h1 class="text-xl font-semibold">${donationQuotaInjurd} Taka <span>${historyText3}</span> </h1>
+     <p class="text-gray-500 text-xs">Date: ${new Date().toDateString()}
+        <span>${new Date().toTimeString()}</span></p>
+     `;
+  historyContainer.appendChild(historyContent);
 });
 
-// History Button in JS
-const donationBtnEl = document.getElementById("donation-btn");
-const gridContainerEl = document.getElementById("grid-container");
+// History Button
 const historyBtnEl = document.getElementById("history-btn");
-// classList add and button hidden JS
+const donationBtnEl = document.getElementById("donation-btn");
 historyBtnEl.addEventListener("click", function () {
-  historyBtnEl.className =
-    "bg-primary border p-2 text-xl font-semibold rounded-md";
+  historyContainer.classList.remove("hidden");
+  gridContainerEl.classList.add("hidden");
+  historyBtnEl.classList.add("bg-primary");
   donationBtnEl.classList.remove("bg-primary");
   donationBtnEl.classList.add("border");
-  gridContainerEl.classList.add("hidden");
-  historyContainer.classList.remove("hidden");
-  // History
-  const historyItemsEl = document.createElement("div");
-  historyItemsEl.className = "border rounded-md p-5 space-y-3";
-  historyItemsEl.innerHTML = `
-   <h1 class="text-xl font-semibold">
-      ${donationFlood}Taka is Donated for famine-2024 at Feni, Bangladesh
-   </h1>
-   <p class="text-gray-500 text-xs"></p>
-  `;
-  const historyContainer = document.getElementById("history-container");
-  historyContainer.insertBefore(historyItemsEl, historyContainer.firstChild);
 });
 
-// Dnation Button In JS
+// Donation Button
+
 donationBtnEl.addEventListener("click", function () {
-  historyContainer.classList.add("hidden");
   gridContainerEl.classList.remove("hidden");
-  donationBtnEl.classList =
-    "bg-primary border p-2 text-xl font-semibold rounded-md";
+  historyContainer.classList.add("hidden");
   historyBtnEl.classList.remove("bg-primary");
+  donationBtnEl.classList.add("bg-primary");
 });
-
-// Utilities Function in JS
-
-// document.getElementById('')
---
+++ js/toggle.js
@@ -0,0 +1,10 @@
+// Blog Button
+const blogBtnEl = document.getElementById("blog-btn");
+blogBtnEl.addEventListener("click", function () {
+  window.location.href = "blog.html";
+});
+// Home Button
+
+document.getElementById("home-btn").addEventListener("click", function () {
+  window.location.href = "index.html";
+});
